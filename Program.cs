277e9fa779d96f3684305503d1601f53cd1eeb34
Cryptographer <85350190+khalilDup@users.noreply.github.com>
2021-12-18 01:18:02 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Requêtes_Faciles_4__.NET_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Récupérer tous les fruits dont la taille du nom est supérieure à 5 lettres
+
+            List<string> Fruits = new List<string>
+            {
+                "Banana", "Peer", "watermelon", "Apple", "Orange"
+            };
+
+            var length = from fr 
+                         in Fruits 
+                         where fr.Length > 5 
+                         orderby fr ascending
+                         select fr;
+
+            foreach (var fr in length)
+            {
+                Console.WriteLine(fr);
+            }
+        }
+    }
+}
